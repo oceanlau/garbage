c493197d34b2ca31a3ea4d511e4ebd14b563e211
oceanlau <liuy.ocean@gmail.com>
2018-09-18 17:59:38 +0800

    0003 fix

--- LeetCode/0003_Longest Substring_Without_Repeating_Characters.cpp
+++ LeetCode/0003_Longest Substring_Without_Repeating_Characters.cpp
@@ -1,41 +1,31 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int string_length = s.length();
         unordered_map<char, int> char_map;
-
-        int i = 0;
         int current_length = 0;
         int longest_length = 0;
-        int first_occurence_location = -1;
-        int last_occurence_location = -1;
-        while (s[i] != '\0') {
-            if (char_map.find(s[i]) != char_map.end()) {
-                if (first_occurence_location == -1) {
-                    first_occurence_location = i;
-                }
-                if (char_map[s[i]] > last_occurence_location) {
-                    last_occurence_location = char_map[s[i]];
-                }
-                current_length = i - char_map[s[i]];
+        int substring_left_pos = 0;
+        int substring_right_pos = 0;
+
+        while (s[substring_right_pos] != '\0') {
+            if (char_map.find(s[substring_right_pos]) != char_map.end()) {
+                current_length = substring_right_pos - substring_left_pos;
                 if (current_length > longest_length) {
                     longest_length = current_length;
                 }
+
+                if (char_map[s[substring_right_pos]] + 1 > substring_left_pos) {
+                    substring_left_pos = char_map[s[substring_right_pos]] + 1;
+                }
             }
-            char_map[s[i]] = i;
-            i++;
+
+            char_map[s[substring_right_pos]] = substring_right_pos;
+            substring_right_pos++;
         }
 
-        if (first_occurence_location > longest_length) {
-            longest_length = first_occurence_location;
-        }
-
-        if (last_occurence_location != -1 && string_length - 1 - last_occurence_location > longest_length) {
-            longest_length = string_length - 1 - last_occurence_location;
-        }
-
-        if (longest_length == 0) {
-            longest_length = string_length;
+        current_length = substring_right_pos - substring_left_pos;
+        if (current_length > longest_length) {
+            longest_length = current_length;
         }
 
         return longest_length;
