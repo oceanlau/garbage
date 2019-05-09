3685f37afc077aed9027375149e0c8b22f06105c
oceanlau <liuy.ocean@gmail.com>
2019-05-09 21:58:15 +0800

    leetcode 10

--- LeetCode/0010_Regular_Expression_Matching.cpp
+++ LeetCode/0010_Regular_Expression_Matching.cpp
@@ -1,4 +1,32 @@
-// mistake
+// recursive
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        
+        if (p.length() < 2) {
+            if (s == p) {
+                return true;
+            } else if ("." == p && s.length() == 1) {
+                return true;
+            }
+            return false;
+        } else if ('*' == p[1]) {
+            if (s.length() != 0 && (s[0] == p[0] || '.' == p[0])) {
+                return isMatch(s.substr(1), p) || isMatch(s, p.substr(2));
+            }
+            return isMatch(s, p.substr(2));
+        } else {
+            if (s.length() != 0 && (s[0] == p[0] || '.' == p[0])) {
+                return isMatch(s.substr(1), p.substr(1));
+            }
+            return false;
+        }
+        
+        
+    }
+};
+
+// mistake, partial matching is not allowed
 class Solution {
 public:
     bool isMatch(string s, string p) {
