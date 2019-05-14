b6a4a58b8ff67234d5637742a8d861a9ba7ed371
oceanlau <liuy.ocean@gmail.com>
2019-05-14 22:05:59 +0800

    lc 14

--- LeetCode/0014_Longest_Common_Prefix.cpp
+++ LeetCode/0014_Longest_Common_Prefix.cpp
@@ -1,11 +1,9 @@
-// Brute
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
         if (0 == strs.size()) {
             return "";
         }
-        string lcp = "";
         int i = 0;
         char last_c = '\0';
         bool is_end = false;
@@ -22,10 +20,39 @@ public:
             if (is_end) {
                 break;
             }
-            lcp += last_c;
             i++;
         }
-        return lcp;
+        return strs[0].substr(0, i);
         
+    }
+};
+
+// Big Perf Boost
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int size_strs = strs.size();
+        if (0 == size_strs) {
+            return "";
+        }
+        int i = 0;
+        char last_c = '\0';
+        bool is_end = false;
+        while (true) {
+            last_c = '\0';
+            for (int j = 0; j < size_strs; j++) {
+                if (strs[j].length() > i && (last_c == '\0' || last_c == strs[j][i])) {
+                    last_c = strs[j][i];
+                } else {
+                    is_end = true;
+                    break;
+                }
+            }
+            if (is_end) {
+                break;
+            }
+            i++;
+        }
+        return strs[0].substr(0, i);
     }
 };
