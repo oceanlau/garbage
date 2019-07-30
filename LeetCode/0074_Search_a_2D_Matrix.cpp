45ecfd40954ff10b71713bd031b7e49bf3ead3b4
oceanlau <liuy.ocean@gmail.com>
2019-07-30 11:32:23 +0800

    lc 73 74

---
+++ LeetCode/0074_Search_a_2D_Matrix.cpp
@@ -0,0 +1,33 @@
+// 56% 5%
+class Solution {
+private:
+    bool _binary_search(vector<int> row, int target) {
+        int mid_pos = row.size() / 2;
+        int mid = row[mid_pos];
+        if (mid == target) {
+            return true;
+        } else if (mid > target && row.begin() + mid_pos < row.end() && mid_pos != 0) {
+            return _binary_search(vector<int>(row.begin(), row.begin() + mid_pos), target);
+        } else if (mid < target && row.begin() + mid_pos + 1 < row.end()){
+            return _binary_search(vector<int>(row.begin() + mid_pos + 1, row.end()), target);
+        }
+        return false;
+    }
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if (matrix.size() == 0 || matrix[0].size() == 0) {
+            return false;
+        }
+        int end = matrix[0].size() - 1;
+        for (int i = 0; i < matrix.size(); i++) {
+            if (matrix[i][0] == target || matrix[i][end] == target) {
+                return true;
+            } else if (matrix[i][0] < target && matrix[i][end] > target) {
+                return _binary_search(matrix[i], target);
+            } else if (matrix[i][0] > target) {
+                return false;
+            }
+        }
+        return false;
+    }
+};
