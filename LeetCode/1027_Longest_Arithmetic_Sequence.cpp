a316ad44cb095170187e543272f1a0ffe31d4e9b
oceanlau <liuy.ocean@gmail.com>
2019-12-03 15:20:36 -0500

    lc

--- LeetCode/1027_Longest_Arithmetic_Sequence.cpp
+++ LeetCode/1027_Longest_Arithmetic_Sequence.cpp
@@ -4,7 +4,9 @@ public:
         int ans = 2;
         vector<unordered_map<int, int>> dp(A.size(), unordered_map<int, int> {}); // pos -> diff -> length(the answer)
         for (int i = 0; i < A.size(); i++) {
-            for (int j = i - 1; j >= 0; j--) {
+            for (int j = 0; j < i; j++) {
+            //the order matters!
+            //for (int j = i - 1; j >= 0; j--) {
                 int diff = A[i] - A[j];
                 if (dp[j].count(diff)) {
                     dp[i][diff] = dp[j][diff] + 1;
